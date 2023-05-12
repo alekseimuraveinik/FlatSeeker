d30865b93955ae816a9fddb7bf5519fe5acde91a
Aleksei Muraveinik <aleksei.muraveinik@gmail.com>
2023-05-13 01:05:41 +0400

    Initial Commit

---
+++ FlatSeeker/FlatSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatSeekerApp.swift
+//  FlatSeeker
+//
+//  Created by Aleksei Muraveinik on 13.05.23.
+//
+
+import SwiftUI
+
+@main
+struct FlatSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
