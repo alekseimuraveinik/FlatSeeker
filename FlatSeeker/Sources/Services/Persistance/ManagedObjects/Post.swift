c8007ab6791556c26de5c66ab390e783fa71cc23
Aleksei Muraveinik <aleksei.muraveinik@gmail.com>
2023-05-22 00:22:16 +0400

    Post automatocally generate properties

--- FlatSeeker/Sources/Services/Persistance/ManagedObjects/Post.swift
+++ FlatSeeker/Sources/Services/Persistance/ManagedObjects/Post.swift
@@ -2,31 +2,20 @@ import CoreData
 
 @objc(Post)
 class Post: NSManagedObject {
-    @nonobjc class func fetchRequest() -> NSFetchRequest<Post> {
-        NSFetchRequest<Post>(entityName: "Post")
-    }
-
-    @NSManaged var id: NSNumber
-    @NSManaged var text: String
-    @NSManaged var price: NSNumber?
-    @NSManaged var district: String?
-}
-
-extension Post {
     convenience init(context: NSManagedObjectContext, post: PostDTO) {
         self.init(context: context)
         
         id = NSNumber(value: post.id)
         text = post.text
-        price = post.price.flatMap(NSNumber.init(value:))
+        price = post.price.map(NSNumber.init(value:))
         district = post.district
     }
     
     var postDTO: PostDTO {
         PostDTO(
-            id: Int(truncating: id),
-            text: text,
-            price: price.flatMap(Int.init(truncating:)),
+            id: Int(truncating: id!),
+            text: text!,
+            price: price.map(Int.init(truncating:)),
             district: district,
             images: []
         )
